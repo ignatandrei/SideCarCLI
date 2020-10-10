6c996df1f02723b79488a4848eb493785532a44e
ignatandrei <ignatandrei@yahoo.com>
2020-10-10 22:27:35 +0300

    Show command line application options

--- src/SideCarCLI/SideCarCLI/Program.cs
+++ src/SideCarCLI/SideCarCLI/Program.cs
@@ -7,11 +7,20 @@ namespace SideCarCLI
     {
         static int Main(string[] args)
         {
-            var app = new CommandLineApplication();
+            var app = new CommandLineApplication()
+            {
+                MakeSuggestionsInErrorMessage = true,
+                FullName = "SideCar for any other application",
+                ResponseFileHandling = ResponseFileHandling.ParseArgsAsLineSeparated,
+                ExtendedHelpText= @"
+                The most simplest form it is: 
+                    SideCarCLI startApp --name YourApp
+                "
+            };
 
-            
+
             app.HelpOption("-h|--help", inherited: true);
-
+            app.Option("-max|--maxSeconds", "max seconds for the StartApp to run", CommandOptionType.SingleOrNoValue); ;
             app.Command("_about", cmd =>
             {
                 cmd.OnExecute(() =>
@@ -19,17 +28,44 @@ namespace SideCarCLI
                     app.ShowVersion();
                     Console.WriteLine("made by Andrei Ignat, http://msprogrammer.serviciipeweb.ro/category/sidecar/");
                 });
-             });
-            app.Command("originalApp", cmd =>
+            });
+            app.Command("startApp", cmd =>
             {
+                cmd.ResponseFileHandling = ResponseFileHandling.ParseArgsAsLineSeparated;
+                cmd.Option("-n|--name <fullPathToApplication>", "Path to the StartApp", CommandOptionType.SingleValue);
+                cmd.Option("-a|--arguments <arguments_to_the_app>", "StartApp arguments", CommandOptionType.MultipleValue);
+                cmd.Option("-f|--folder <folder_where_execute_the_app>", "folder where to execute the StartApp - default folder of the StartApp ", CommandOptionType.SingleOrNoValue);
+            });
 
-            }
-
-
-                app.OnExecute(() =>
+            app.Command("lineInterceptors", cmd =>
             {
-                Console.WriteLine("Specify a subcommand");
-                app.ShowVersion();
+                cmd.ResponseFileHandling = ResponseFileHandling.ParseArgsAsLineSeparated;
+                cmd.Option("-l|--list", "List interceptors for lines", CommandOptionType.NoValue);                                
+                cmd.Option("-a|--add", "Add interceptor to execute", CommandOptionType.MultipleValue);
+                cmd.Option("-f|--folder", "folder where to start the interceptor" ,CommandOptionType.SingleOrNoValue);
+            });
+
+            app.Command("timer", cmd =>
+            {
+                cmd.ResponseFileHandling = ResponseFileHandling.ParseArgsAsLineSeparated;
+                cmd.Option("-i|--intervalRepeatSeconds", "Repeat interval in seconds", CommandOptionType.SingleValue);
+                cmd.Option("-l|--list", "List interceptors to execute periodically", CommandOptionType.NoValue);
+                cmd.Option("-a|--add", "Add interceptor to execute", CommandOptionType.MultipleValue);
+                cmd.Option("-f|--folder", "folder where to start the interceptor" ,CommandOptionType.SingleOrNoValue);
+            
+            });
+            app.Command("finishInterceptors", cmd =>
+            {
+                cmd.ResponseFileHandling = ResponseFileHandling.ParseArgsAsLineSeparated;
+                cmd.Option("-l|--list", "List interceptors for finish application", CommandOptionType.NoValue);
+                cmd.Option("-a|--add", "Add interceptor to execute", CommandOptionType.MultipleValue);
+                cmd.Option("-f|--folder", "folder where to start the interceptor", CommandOptionType.SingleOrNoValue);
+
+            });
+
+            app.OnExecute(() =>
+            {
+                Console.WriteLine("Specify a command");
                 app.ShowHelp();
                 return 1;
             });
